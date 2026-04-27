a3a3d46ae63ad30da334fdb8dec07d8d02d713f4
cpg716 <cpg716@gmail.com>
2026-04-27 00:12:58 -0400

    Bootstrap staff_error_event status schema at startup

--- server/src/schema_bootstrap.rs
+++ server/src/schema_bootstrap.rs
@@ -6,11 +6,16 @@ use sqlx::PgPool;
 const STAFF_RBAC_SQL: &str = include_str!("../../migrations/34_staff_contacts_and_permissions.sql");
 const CP_FINISHING_SQL: &str =
     include_str!("../../migrations/114_counterpoint_historical_finishing.sql");
+const STAFF_ERROR_EVENT_STATUS_BOOTSTRAP_SQL: &str =
+    include_str!("../../migrations/170_staff_error_event_status_compat.sql");
 
 /// Ensures core database schema requirements are met (RBAC, Counterpoint Finishing, etc).
 /// Safe to call on every startup.
 pub async fn ensure_core_schema(pool: &PgPool) -> Result<(), sqlx::Error> {
     sqlx::raw_sql(STAFF_RBAC_SQL).execute(pool).await?;
     sqlx::raw_sql(CP_FINISHING_SQL).execute(pool).await?;
+    sqlx::raw_sql(STAFF_ERROR_EVENT_STATUS_BOOTSTRAP_SQL)
+        .execute(pool)
+        .await?;
     Ok(())
 }
